6fb51364a959c985e658642da29e4493861ff109
Vicky Chhetri <specialpower321@gmail.com>
2024-01-25 01:09:47 +0530

    login , show dialog message

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -40,6 +40,14 @@
         <meta property="twitter:description" content="Obstetric emergencies are health problems that are life-threatening for pregnant women and their babies.">
         <meta property="twitter:image" content="https://obstetricalemergencies.in/images/mother.png">
 
+
+        <style>
+            /* Add custom CSS to make modal backdrop transparent */
+            .modal-backdrop {
+                opacity: 0.7; /* Adjust the opacity as needed */
+            }
+        </style>
+
         <script>
             addEventListener(
                 "load",
@@ -81,6 +89,7 @@
 
     <body>
         <section class="ftco-section p-4">
+
             <div class="container">
                 <div class="row justify-content-center">
                     <div class="col-md-6 text-center mb-5">
@@ -174,11 +183,90 @@
     <div class="copyright-w3layouts py-xl-3 py-2 mt-xl-5 mt-4 text-center">
         <p>© 2024 Learn Obstetrical Emergencies . All Rights Reserved | Develop by Simarjeet Kaur  <a href="/privacy-policy"> Privacy Policy</a>
         </p>
+{{--        <div class="overlay" id="welcomeOverlay">--}}
+{{--            <div class="modal bg-danger">--}}
+{{--                <h2 class="text-dark">Welcome to Our Learning Platform!</h2>--}}
+{{--                <p>We're glad you're here. Explore our Obstetrical Emergencies learning content.</p>--}}
+{{--                <button class="close-btn" onclick="closeWelcomeModal()">Close</button>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+
+        <!-- Button trigger modal -->
+
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+
+        <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+                <div class="modal-content">
+                    <div class="modal-header d-flex justify-content-center">
+                        <h5 class="modal-title text-center" id="exampleModalLabel">Welcome to Our Learning Platform!</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <h3>  Explore our Obstetrical Emergencies learning content.
+                        </h3>
+                        <p style="text-align: justify">While using this e- learning package, We may ask You to provide Us with certain personally identifiable information that can be used to contact or identify You. Personally identifiable information may includes:
+                            <b>Email address,
+                            First name and last name
+                            Phone number,
+                            Year of study,
+                            Institute name </b>
+                            The researcher will retain your Personal Data only for as long as is necessary for the purposes of research and confidentiality of your data and identity will be maintained. We will retain and use Your Data to the extent necessary to comply with our legal obligations (for example, if we are required to retain your data to comply with applicable laws), resolve disputes, and enforce our legal agreements and policies.
+
+                            The researcher will also retain collected Data for  analysis and interpretation purpose.
+                            Your consent to this Privacy Policy followed by Your submission of such information represents Your agreement to the research study.</p>
+
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
     </div>
-    <!--// Copyright -->
-        <!-- <script src="OuterArea/js/jquery.min.js"></script>
+        <!-- Modal code -->
+
+        <script>
+
+            function checkFirstVisit() {
+                return !localStorage.getItem('visitedBefore');
+            }
+            function closeWelcomeModal() {
+                localStorage.setItem('visitedBefore', 'true');
+            }
+
+            window.addEventListener('load', function () {
+                if (checkFirstVisit()) {
+                    $('#exampleModal').modal('show');
+                    closeWelcomeModal();
+                }
+
+                $('#exampleModal').on('click', function (e) {
+                    if ($(e.target).hasClass('modal')) {
+                        return false;
+                    }
+                });
+
+
+                if(checkFirstVisit()){
+                    $('#exampleModal').modal({
+                        backdrop: 'static',  // Prevents closing by clicking outside the modal
+                        keyboard: false      // Prevents closing by pressing the escape key
+                    });
+                }
+            });
+
+        </script>
+
+
+  <script src="OuterArea/js/jquery.min.js"></script>
   <script src="OuterArea/js/popper.js"></script>
   <script src="OuterArea/js/bootstrap.min.js"></script>
-  <script src="OuterArea/js/main.js"></script> -->
+  <script src="OuterArea/js/main.js"></script>
+
+
+
     </body>
 </html>
